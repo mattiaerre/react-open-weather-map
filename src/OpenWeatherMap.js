2c53d65d2326ae99ab940f6565e6a7fed039b922
Philip Kirkbride <phil.kirkbride@virtualhighschool.com>
2017-01-27 13:49:08 +0000

    revert last changes and change sample data

--- src/OpenWeatherMap.js
+++ src/OpenWeatherMap.js
@@ -5,17 +5,17 @@ const OpenWeatherMap = ({ data, config }) => {
   const containerClassName = (config && config.containerClassName) ? config.containerClassName : 'react-open-weather-map'
   if (data) {
     const day = moment.unix(data.dt)
-    const src = `http://openweathermap.org/img/w/${data.weather.weather[0].icon}.png`
+    const src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
 
     return (
       <div className={containerClassName}>
-        <div className="main-heading">{data.name}, {data.weather.sys.country}</div>
+        <div className="main-heading">{data.name}, {data.sys.country}</div>
         <div className="day">{day.format('dddd h:mm A') }</div>
-        <div className="description">{data.weather.weather[0].description}</div>
+        <div className="description">{data.weather[0].description}</div>
         <div className="icon">
-          <img src={src} alt={data.weather.weather[0].description} />
+          <img src={src} alt={data.weather[0].description} />
         </div>
-        <div className="temperature">{data.weather.main.temp} &deg;C</div>
+        <div className="temperature">{data.main.temp} &deg;C</div>
       </div>
     )
   }
